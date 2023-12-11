77d0883e9dcc61e5b89e30e7c996b3f6f617e164
Chaitali Desale <chaitali.desale92@gmail.com>
2023-12-11 15:37:36 +0000

    Updated header style

--- components/header.jsx
+++ components/header.jsx
@@ -2,13 +2,14 @@ export const Header = () => {
   return (
     <div
       style={{
+        marginBottom: "16px",
         display: "flex",
         justifyContent: "center",
         padding: "0.5rem",
         borderBottom: "0.1rem solid black",
       }}
     >
-      NC News
+      <h1 style={{ margin: "0px" }}>NC News</h1>
     </div>
   );
 };
