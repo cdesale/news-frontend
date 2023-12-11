a1ac5e3a6681438cd32eff5c5be2262a318855a6
Chaitali Desale <chaitali.desale92@gmail.com>
2023-12-11 12:48:32 +0000

    Adds header component alongwith some styling

---
+++ components/header.jsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        padding: "0.5rem",
+        borderBottom: "0.1rem solid black",
+      }}
+    >
+      NC News
+    </div>
+  );
+};
