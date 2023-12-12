de76b840ead8634c3a6877febc95c8f84f1f930d
Chaitali Desale <chaitali.desale92@gmail.com>
2023-12-12 14:09:20 +0000

    Adds topic wise article search WIP

---
+++ components/search-container.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export const SearchContainer = () => {
+  const [topics, setTopics] = useState();
+
+  return (
+    <div class="dropdown" style={{ marginBottom: "100px" }}>
+      <button
+        class="btn btn-secondary dropdown-toggle"
+        type="button"
+        id="dropdownMenuButton"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
+        Dropdown button
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="#">
+          Action
+        </a>
+        <a class="dropdown-item" href="#">
+          Another action
+        </a>
+        <a class="dropdown-item" href="#">
+          Something else here
+        </a>
+      </div>
+    </div>
+  );
+};
